2b484bb70eb37a3f6e0dc3bbbd22fe9c0346d4e2
Christian Raoulis <CRaoulis@googlemail.com>
2023-05-25 21:03:26 +0200

    1.19 update

--- forge/src/main/java/de/blutmondgilde/cloudsettings/forge/EventHandler.java
+++ forge/src/main/java/de/blutmondgilde/cloudsettings/forge/EventHandler.java
@@ -3,7 +3,7 @@ package de.blutmondgilde.cloudsettings.forge;
 import de.blutmondgilde.cloudsettings.CloudSettings;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ScreenOpenEvent;
+import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @EventBusSubscriber(bus = Bus.FORGE, value = Dist.CLIENT)
 public class EventHandler {
     @SubscribeEvent
-    public static void onMainMenu(final ScreenOpenEvent e) {
+    public static void onMainMenu(final ScreenEvent.Opening e) {
         if (e.getScreen() instanceof TitleScreen) {
             CloudSettings.titleScreenOpened();
         }
