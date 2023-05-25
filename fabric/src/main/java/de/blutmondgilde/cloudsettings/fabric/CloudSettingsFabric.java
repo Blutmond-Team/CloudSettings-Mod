07b3a1104b1088df2dcb91e6e53975f4880450a2
Christian Raoulis <CRaoulis@googlemail.com>
2023-05-25 08:36:14 +0200

    add fabric entry point

--- fabric/src/main/java/de/blutmondgilde/cloudsettings/fabric/CloudSettingsFabric.java
+++ fabric/src/main/java/de/blutmondgilde/cloudsettings/fabric/CloudSettingsFabric.java
@@ -2,17 +2,22 @@ package de.blutmondgilde.cloudsettings.fabric;
 
 import de.blutmondgilde.cloudsettings.CloudSettings;
 import de.blutmondgilde.cloudsettings.IPlatformHandler;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CloudSettingsFabric implements ModInitializer, IPlatformHandler {
+public class CloudSettingsFabric implements ClientModInitializer, IPlatformHandler {
     public static final Logger LOGGER = LogManager.getLogger("CloudSettings");
 
     @Override
-    public void onInitialize() {
+    public Logger getLogger() {
+        return LOGGER;
+    }
+
+    @Override
+    public void onInitializeClient() {
         CloudSettings.init(this);
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             if (screen instanceof TitleScreen) {
@@ -21,10 +26,5 @@ public class CloudSettingsFabric implements ModInitializer, IPlatformHandler {
                 LOGGER.info("Screen: {}", screen.getClass());
             }
         });
-    }
-
-    @Override
-    public Logger getLogger() {
-        return LOGGER;
     }
 }
