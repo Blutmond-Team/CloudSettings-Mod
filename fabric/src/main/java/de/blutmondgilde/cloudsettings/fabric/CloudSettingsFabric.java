b959e97fb0e175ff9d94366c94ad5b9b2247fe99
Christian Raoulis <CRaoulis@googlemail.com>
2023-05-26 12:40:59 +0200

    update 2.0.0.1

--- fabric/src/main/java/de/blutmondgilde/cloudsettings/fabric/CloudSettingsFabric.java
+++ fabric/src/main/java/de/blutmondgilde/cloudsettings/fabric/CloudSettingsFabric.java
@@ -3,9 +3,7 @@ package de.blutmondgilde.cloudsettings.fabric;
 import de.blutmondgilde.cloudsettings.CloudSettings;
 import de.blutmondgilde.cloudsettings.IPlatformHandler;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.gui.screens.TitleScreen;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,10 +23,5 @@ public class CloudSettingsFabric implements ClientModInitializer, IPlatformHandl
     @Override
     public void onInitializeClient() {
         CloudSettings.init(this);
-        ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
-            if (screen instanceof TitleScreen) {
-                CloudSettings.titleScreenOpened();
-            }
-        });
     }
 }
