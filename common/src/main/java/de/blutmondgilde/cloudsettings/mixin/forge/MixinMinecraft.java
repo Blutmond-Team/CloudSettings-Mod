0e7104258a22cd40c1916264ee195dc8c20c27f3
Christian Raoulis <CRaoulis@googlemail.com>
2023-05-26 12:40:59 +0200

    update 2.0.0.1

---
+++ common/src/main/java/de/blutmondgilde/cloudsettings/mixin/forge/MixinMinecraft.java
@@ -0,0 +1,17 @@
+package de.blutmondgilde.cloudsettings.mixin.forge;
+
+import de.blutmondgilde.cloudsettings.CloudSettings;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Overlay;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    @Inject(method = "setOverlay", at = @At(value = "HEAD"))
+    public void beforeTitleScreen(Overlay overlay, CallbackInfo ci) {
+        CloudSettings.titleScreenOpened();
+    }
+}
